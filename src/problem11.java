4d45ce6fac7e1505dee8431bfcffb1477635cb3f
anuLoveCoading <anusrivas07@gmail.com>
2022-11-23 23:47:11 +0530

    git commit -m "2D ArrayProblem"

--- src/problem11.java
+++ src/problem11.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class problem11 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello 1");
+        Scanner scn= new Scanner(System.in);
+//        (Q.1) You are given a N X N square integer matrix A.
+//        You have to tell whether A is an identity matrix or not.
+        int n = scn.nextInt();
+        int [][] arr = new int[n][n];
+
+        for(int i=0;i<arr.length;i++){
+            for (int j = 0; j <arr[0].length ; j++) {
+                arr[i][j]=scn.nextInt();
+            }
+        }
+        int ans = 0;
+        if(arr.length!= arr[0].length){
+            ans =0;
+        }
+
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr[0].length ; j++) {
+                if(i==j && arr[i][j]!=1){
+                    ans=0;
+                }
+                if(i!=j && arr[i][j]!=0){
+                    ans =0;
+                }
+            }
+
+        }
+        ans =1;
+
     }
 }
