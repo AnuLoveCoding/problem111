443d4c4c0991f752753ecbec23ca4860aa5c1cd4
anuLoveCoading <anusrivas07@gmail.com>
2022-11-25 23:29:49 +0530

    git commit -m "2D ArrayProblem"

--- src/problem13.java
+++ src/problem13.java
@@ -1,5 +1,22 @@
-public class problem13 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class problem13 {
+    public static int fabonnci_number(int n){
+        int a =0,b=1,c;
+        for(int i=1;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+
+        return a ;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Your number");
+        int n = scn.nextInt();
+
+        System.out.println("The result of fabonnci number is "+fabonnci_number(n));
     }
 }
--- src/problem14.java
+++ src/problem14.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class problem14 {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+//        (Q.1) Given an integer A, you need to find the count of it's factors.
+        int n=scn.nextInt();
+        int count =0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                if(i==n/i){
+                    count++;
+                }else{
+                    count+=2;
+                }
+            }
+        }
+
+        System.out.println(count);
 
     }
 }
