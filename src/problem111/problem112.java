45ba6bd5f4e8f2989b844978c6d40e1804aa749b
anuLoveCoading <anusrivas07@gmail.com>
2022-11-28 00:28:01 +0530

    git commit -m "2D ArrayProblem"

--- src/problem111/problem112.java
+++ src/problem111/problem112.java
@@ -1,4 +1,26 @@
 package problem111;
 
+import java.util.Scanner;
+
 public class problem112 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        String sentence = scn.nextLine();
+
+        String vowel ="";
+        String consonant = "";
+
+        for (int i = 0; i <sentence.length() ; i++) {
+            char ch = sentence.charAt(i);
+            if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vowel+=ch;
+            }else{
+                consonant+=ch;
+            }
+        }
+
+        System.out.println(vowel+" ");
+        System.out.println(consonant+" ");
+    }
 }
--- src/problem16.java
+++ src/problem16.java
@@ -5,39 +5,43 @@ public class problem16 {
         Scanner scn = new Scanner(System.in);
 
         int n = scn .nextInt();
-        int [][] arr = new int[n][n];
 
-        for(int i=0;i<arr.length;i++){
-            for (int j = 0; j <arr[0].length ; j++) {
-                arr[i][j]=scn.nextInt();
-            }
+        for (int i = 0; i < n; i++) {
+            
         }
-
-        int sum = 0;
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(i==j){
-                    sum+=arr[i][j];
-                }
-
-                if(i+j==n-1){
-                    sum+=arr[i][j];
-                }
-
-            }
-
-            if(n%2==1){
-                sum-=arr[n/2][n/2];
-            }
-        }
-
-        sum-=arr[0][0];
-        sum-=arr[0][n-1];
-        sum-=arr[n-1][0];
-        sum-=arr[n-1][n-1];
-
-        System.out.println(sum);
+//        int [][] arr = new int[n][n];
+//
+//        for(int i=0;i<arr.length;i++){
+//            for (int j = 0; j <arr[0].length ; j++) {
+//                arr[i][j]=scn.nextInt();
+//            }
+//        }
+//
+//        int sum = 0;
+//
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<arr[0].length;j++){
+//                if(i==j){
+//                    sum+=arr[i][j];
+//                }
+//
+//                if(i+j==n-1){
+//                    sum+=arr[i][j];
+//                }
+//
+//            }
+//
+//            if(n%2==1){
+//                sum-=arr[n/2][n/2];
+//            }
+//        }
+//
+//        sum-=arr[0][0];
+//        sum-=arr[0][n-1];
+//        sum-=arr[n-1][0];
+//        sum-=arr[n-1][n-1];
+//
+//        System.out.println(sum);
 
 
     }
