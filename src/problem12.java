ac5040699f1dba9d5cfd7ec68ab60101e4c8741d
anuLoveCoading <anusrivas07@gmail.com>
2022-11-28 19:34:02 +0530

    git commit -m "2D ArrayProblem"

--- src/problem12.java
+++ src/problem12.java
@@ -1,51 +1,94 @@
 import java.util.Scanner;
 
 public class problem12 {
-    public static int [] first_last(int [] arr ,int b){
-        int [] c= new int [2];
+//    public static int [] first_last(int [] arr ,int b){
+//        int [] c= new int [2];
+//
+//        int fi =-1;
+//        int li =-1;
+//
+//        for (int i = 0; i <arr.length ; i++) {
+//            if(arr[i]==b){
+//                fi = i;
+//                c[0] =fi;
+//                break;
+//            }
+//        }
+//
+//        for (int i = arr.length-1; i>=0 ; i--) {
+//            if(arr[i]==b){
+//                li = i;
+//                c[1]=li;
+//                break;
+//            }
+//        }
+//
+//        return c;
+//    }
 
-        int fi =-1;
-        int li =-1;
+//    (Q.2)
+    public static int [] insert_element(int [] arr ,int x ,int y){
+        int n = arr.length;
+        int [] ans = new int [n+1];
 
-        for (int i = 0; i <arr.length ; i++) {
-            if(arr[i]==b){
-                fi = i;
-                c[0] =fi;
-                break;
-            }
+        for (int i = 0; i < x ; i++) {
+            ans[i]=arr[i];
         }
 
-        for (int i = arr.length-1; i>=0 ; i--) {
-            if(arr[i]==b){
-                li = i;
-                c[1]=li;
-                break;
-            }
-        }
+        ans[x] = y ;
 
-        return c;
+        for (int i = x+1; i <ans.length; i++) {
+            ans[i] = arr[i - 1];
+
+        }
+        return  ans;
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
 //        (Q.1) find the first_index and last_index of array;
-            System.out.println("Enter Your arr size");
-            int n = scn.nextInt();
-            int [] arr = new int [n];
+//            System.out.println("Enter Your arr size");
+//            int n = scn.nextInt();
+//            int [] arr = new int [n];
+//
+//            System.out.println("Enter your arr value");
+//            for (int i = 0; i < arr.length ; i++) {
+//                arr[i]= scn.nextInt();
+//            }
+//
+//            System.out.println("Enter your search value");
+//            int b = scn.nextInt();
+//
+//            int [] arr1 = first_last(arr,b);
+//
+//            for (int i = 0; i <arr1.length ; i++) {
+//            System.out.print(arr1[i]+" ");
+//         }
 
-            System.out.println("Enter your arr value");
-            for (int i = 0; i < arr.length ; i++) {
-                arr[i]= scn.nextInt();
+//       (Q.2) Write a program to input N numbers array, a number X and a number Y
+//       from user and insert an element Y in it at specified position X. X is based on 1-based indexing.
+
+              System.out.println("Enter your arr size");
+              int n  = scn.nextInt();
+              int [] arr = new int [n];
+              int [] ans = new int [n+1];
+
+                 System.out.println("Enter your arr value");
+                for (int i = 0; i < arr.length ; i++) {
+                    arr[i] = scn.nextInt();
+                }
+
+             System.out.println("Enter your insert index");
+            int x = scn.nextInt()-1;
+            System.out.println("Enter your insert value");
+            int y = scn.nextInt();
+
+            int [] ans1 = insert_element(arr,x,y);
+
+            for (int i = 0; i <ans1.length ; i++) {
+                System.out.print(ans1[i]+" ");
             }
 
-            System.out.println("Enter your search value");
-            int b = scn.nextInt();
-
-            int [] arr1 = first_last(arr,b);
-
-            for (int i = 0; i <arr1.length ; i++) {
-            System.out.print(arr1[i]+" ");
-        }
 
     }
 }
