667081cb5cea1c5009f0ad420411c604c69cdd09
anuLoveCoading <anusrivas07@gmail.com>
2022-11-28 20:00:33 +0530

    git commit -m "2D ArrayProblem"

--- src/problem12.java
+++ src/problem12.java
@@ -26,23 +26,23 @@ public class problem12 {
 //        return c;
 //    }
 
-//    (Q.2)
-    public static int [] insert_element(int [] arr ,int x ,int y){
-        int n = arr.length;
-        int [] ans = new int [n+1];
-
-        for (int i = 0; i < x ; i++) {
-            ans[i]=arr[i];
-        }
-
-        ans[x] = y ;
-
-        for (int i = x+1; i <ans.length; i++) {
-            ans[i] = arr[i - 1];
-
-        }
-        return  ans;
-    }
+//    (Q.2)  Insert that
+//    public static int [] insert_element(int [] arr ,int x ,int y){
+//        int n = arr.length;
+//        int [] ans = new int [n+1];
+//
+//        for (int i = 0; i < x ; i++) {
+//            ans[i]=arr[i];
+//        }
+//
+//        ans[x] = y ;
+//
+//        for (int i = x+1; i <ans.length; i++) {
+//            ans[i] = arr[i - 1];
+//
+//        }
+//        return  ans;
+//    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
@@ -65,30 +65,50 @@ public class problem12 {
 //            System.out.print(arr1[i]+" ");
 //         }
 
-//       (Q.2) Write a program to input N numbers array, a number X and a number Y
+//       (Q.2) Insert that <>(); Write a program to input N numbers array, a number X and a number Y
 //       from user and insert an element Y in it at specified position X. X is based on 1-based indexing.
 
-              System.out.println("Enter your arr size");
-              int n  = scn.nextInt();
-              int [] arr = new int [n];
-              int [] ans = new int [n+1];
+//              System.out.println("Enter your arr size");
+//              int n  = scn.nextInt();
+//              int [] arr = new int [n];
+//              int [] ans = new int [n+1];
+//
+//                 System.out.println("Enter your arr value");
+//                for (int i = 0; i < arr.length ; i++) {
+//                    arr[i] = scn.nextInt();
+//                }
+//
+//             System.out.println("Enter your insert index");
+//            int x = scn.nextInt()-1;
+//            System.out.println("Enter your insert value");
+//            int y = scn.nextInt();
+//
+//            int [] ans1 = insert_element(arr,x,y);
+//
+//            for (int i = 0; i <ans1.length ; i++) {
+//                System.out.print(ans1[i]+" ");
+//            }
 
-                 System.out.println("Enter your arr value");
-                for (int i = 0; i < arr.length ; i++) {
-                    arr[i] = scn.nextInt();
-                }
+//        (Q.3)  Right Shift an Array </>;
+            int n = scn.nextInt();
+            int [] arr = new int [n];
 
-             System.out.println("Enter your insert index");
-            int x = scn.nextInt()-1;
-            System.out.println("Enter your insert value");
-            int y = scn.nextInt();
-
-            int [] ans1 = insert_element(arr,x,y);
-
-            for (int i = 0; i <ans1.length ; i++) {
-                System.out.print(ans1[i]+" ");
+            for (int i = 0; i <arr.length ; i++) {
+                arr[i]=scn.nextInt();
             }
 
+            int last_element = arr[arr.length-1];
+
+            for(int i=arr.length-1;i>=1;i--){
+                arr[i] =arr[i-1];
+            }
+
+            arr[0]=last_element;
+
+
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.print(arr[i]+" ");
+        }
 
     }
 }
