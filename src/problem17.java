57afd3351ad63c0682afba3587d78f901a9e1a86
anuLoveCoading <anusrivas07@gmail.com>
2022-12-02 22:38:06 +0530

    git commit -m "2D ArrayProblem"

--- src/problem17.java
+++ src/problem17.java
@@ -1,13 +1,35 @@
+import java.util.Scanner;
+
 public class problem17 {
-    public static void main(String[] args) {
-        for (int i = 0; i <90;i--){
-            System.out.println(i);
 
+    public static int [] solve(int [] arr , int b){
+        int [] arr1 = new int [arr.length];
 
-
-
-
-
+        for (int i = 0; i <arr.length ; i++) {
+            arr[(i+b)%arr.length]=arr[i];
         }
+
+        return arr1;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int [] arr = new int[n];
+
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int b = scn.nextInt();
+
+        int [] ans = solve(arr,b);
+
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+
+
     }
 }
