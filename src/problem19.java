cdb4b8051f0970beca192e8c1c1c1e3d97bd24b4
anuLoveCoading <anusrivas07@gmail.com>
2022-12-02 23:15:06 +0530

    git commit -m "2D ArrayProblem"

---
+++ src/problem19.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class problem19 {
+    public static int max_min(int [] arr){
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+
+        int diff = max + min;
+
+        return diff;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int [] arr = new int[n];
+
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println(max_min(arr));
+
+    }
+}
