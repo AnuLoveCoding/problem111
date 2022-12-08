18627571ec0bc140aec8e3125bdee78f7050cf00
anuLoveCoading <anusrivas07@gmail.com>
2022-12-08 23:47:29 +0530

    git commit -m "2D ArrayProblem"

--- src/problem44.java
+++ src/problem44.java
@@ -1,23 +1,34 @@
 public class problem44 {
-    public int[] solve(int[] A) {
-        int [] arr = new int [A.length];
-
-        int product = 1;
-
-
-        for(int i=0;i<A.length;i++){
-            product*=A[i];
-        }
-
-        for(int i=0;i<arr.length;i++){
-            int divide = product;
-            divide/=A[i];
-            arr[i]=divide;
-        }
-
-        return arr;
-    }
+//    public int[] solve(int[] A) {
+//        int [] arr = new int [A.length];
+//
+//        int product = 1;
+//
+//
+//        for(int i=0;i<A.length;i++){
+//            product*=A[i];
+//        }
+//
+//        for(int i=0;i<arr.length;i++){
+//            int divide = product;
+//            divide/=A[i];
+//            arr[i]=divide;
+//        }
+//
+//        return arr;
+//    }
     public static void main(String[]args){
 
+    }
+    public int[] solve(int[] A) {
+        int [] prefix_sum = new int[A.length];
+
+        prefix_sum[0] = A[0];
+
+        for(int i=1;i<A.length;i++){
+            prefix_sum[i]=prefix_sum[i-1]+A[i];
+        }
+
+        return prefix_sum;
     }
 }
